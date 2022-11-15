7a3e40d7cbf78254c4b9a6e3fbb09a73d29a497b
nimagrolis <486016@gmail.com>
2022-11-15 16:53:12 +0300

    v5.5.9 Update for binexe

--- index.js
+++ index.js
@@ -3,13 +3,18 @@
  */
 const util = require('util');
 
-const plugin = require('ih-plugin-api')();
+// const plugin = require('ih-plugin-api')();
 const app = require('./app');
 
 (async () => {
-  plugin.log('SNMP plugin has started.', 0);
-
+  let plugin;
   try {
+    const opt = getOptFromArgs();
+    const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
+    plugin = require(pluginapi+'/index.js')();
+
+    plugin.log('SNMP plugin has started.', 0);
+
     plugin.params = await plugin.params.get();
     plugin.log('Received params...', 1);
 
@@ -21,3 +26,15 @@ const app = require('./app');
     plugin.exit(8, `Error! Message: ${util.inspect(err)}`);
   }
 })();
+
+
+
+function getOptFromArgs() {
+  let opt;
+  try {
+    opt = JSON.parse(process.argv[2]); //
+  } catch (e) {
+    opt = {};
+  }
+  return opt;
+}
