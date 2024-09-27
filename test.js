899ac96fff69cd5bc02db41b10e5c0ecc6e3a5fe
Артем Иванов <temaivanoff@mail.ru>
2024-09-27 09:28:20 +0300

    release v5.5.22

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const snmp = require('net-snmp');
 
-const session = snmp.createSession('192.168.0.222', 'public', { 
+const session = snmp.createSession('192.168.0.150', 'public', { 
   retries: 0,
   timeout: 5000,
   port: 161, 
+  sourcePort: 161,
   transport: 'udp4',
   version: snmp.Version2c,
-  sourcePort: 163,
 });
 
 session.get(['1.3.6.1.4.1.51014.9.1.9.0'], function(error, varbinds) {
